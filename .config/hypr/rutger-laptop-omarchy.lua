df7a93b7ec13de87e659b8a36af70e2bfa10324a
Rutger Pronk <rutger.pronk11@gmail.com>
2026-09-10 17:51:12 +0200

    Use wayland conf in desktop file for autostart spotify

--- .config/hypr/rutger-laptop-omarchy.lua
+++ .config/hypr/rutger-laptop-omarchy.lua
@@ -11,7 +11,8 @@ hl.on("hyprland.start", function()
   hl.exec_cmd("hyprctl dispatch workspace 4")
   hl.exec_cmd("[workspace 4 silent] uwsm app -- discord")
 
-  hl.exec_cmd("[workspace 5 silent] uwsm app -- spotify")
+  -- Use desktop file, with wayland config
+  hl.exec_cmd("[workspace 5 silent] uwsm app start spotify")
 end)
 
 -- https://wiki.hypr.land/Configuring/Performance/#how-do-i-make-hyprland-draw-as-little-power-as-possible-on-my-laptop
